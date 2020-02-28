e3eae637912395116640f5cb39cba274119bd66f
Petr Kmoch <petr.kmoch@gmail.com>
2020-02-28 21:43:58 +0100

    Add sunrise/sunset calculation

--- python/tools/sun.py
+++ python/tools/sun.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 
 # Copied from https://en.wikipedia.org/wiki/Position_of_the_Sun:
@@ -8,14 +9,15 @@ declination_per_day = math.radians(360)/365.24
 eccentricity = 0.0167
 # perihelion_day = 12
 perihelion_day = 0 # By DM fiat, perihelion occurs on solstice noon (it happened in 1246 AD, so what)
-# ToDo: perihelion moves by 1 day per 58 years, do I want this in?
+# ToDo: perihelion moves by 1 day per 58 years, do I want this in? => No, such accuracy is not necessary
 solstice_declination = math.radians(-23.44)
 
-def declination(time):
+def declination(time, solstice_offset=0):
+    # time is number of days since noon at I.
     return math.degrees(math.asin(
         math.sin(solstice_declination) *
         math.cos(
-            declination_per_day * time +
+            declination_per_day * (time-solstice_offset) +
             math.radians(360)/math.pi * eccentricity * math.sin(declination_per_day * (time - perihelion_day))
         )
     ))
@@ -37,9 +39,24 @@ def time_of_declination(target_declination, time_guess, interval_days=3):
     return best_time
 
 
+def sunset_angle(latitude, declination):
+    return math.degrees(math.acos(-math.tan(math.radians(latitude)) * math.tan(math.radians(declination))))
+
+
+def time_from_angle(angle):
+    return time.gmtime((angle/15 + 12) * 3600)
+
+
+# Notes: 1deg ~ 69 miles ~ 111 km
+
+
 if __name__ == '__main__':
     print("Winter solstice:", time_of_declination(-23.44, 0))
     print("Spring equinox:", time_of_declination(0, 91.25))
     print("Summer solstice:", time_of_declination(23.44, 182.5))
     print("Autumn equinox:", time_of_declination(0, 3*91.25))
     print("Winter solstice:", time_of_declination(-23.44, 365))
+
+    print("Summer solstice sunrise (south):", time.strftime('%H:%M', time_from_angle(-sunset_angle(latitude=50, declination=23.44))))
+    print("Summer solstice sunrise (north):", time.strftime('%H:%M', time_from_angle(-sunset_angle(latitude=55, declination=23.44))))
+    print("Spring equinox sunrise:", time.strftime('%H:%M', time_from_angle(-sunset_angle(latitude=50, declination=0))))
