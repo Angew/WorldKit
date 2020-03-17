2c3ad51a5752d24313d5a792cfa4a9997ce888a9
Petr Kmoch <petr.kmoch@gmail.com>
2020-03-17 17:05:11 +0100

    WIP on diary

--- python/tools/diary.py
+++ python/tools/diary.py
@@ -1,10 +1,22 @@
 import argparse
 import os.path
+import skyfield
 import sys
 
 
+# Julian date = days since 1 Jan -4713
+
+timescale = skyfield.api.load.timescale(builtin=True)
+planets = skyfield.api.load("de406.bsp")  # goes from -3000 to +3000
+
+
 def find_epoch(options):
-    pass
+    skyfield.almanac.find_discrete(
+        timescale.tt_jd(1_000_000)  # Roughly -2000
+        , timescale.tt_jd(2_500_000)  # Roughly +2000
+        , skyfield.almanac.seasons(planets)
+        , epsilon = 1/24/2  # Half-hour precision is good enough
+    )
 
 
 def main(args):
